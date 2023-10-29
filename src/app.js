4acffae902d38e19fe37300d9d84d3d02ece05f9
Mermate <138124998+Mermate@users.noreply.github.com>
2023-10-29 18:30:06 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,61 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const suits = ["♦", "♥", "♠", "♣"];
+const numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  getRandomCard();
+
+  setInterval(window.onload, 5000);
 };
+
+function getRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.getElementById("topSuit").innerHTML = randomSuit;
+  document.getElementById("cardNumber").innerHTML = randomNumber;
+  document.getElementById("bottomSuit").innerHTML = randomSuit;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    document.querySelector("#topSuit").style.color = "red";
+    document.querySelector("#bottomSuit").style.color = "red";
+  } else {
+    document.querySelector("#topSuit").style.color = "black";
+    document.querySelector("#bottomSuit").style.color = "black";
+  }
+
+  return randomSuit + randomSuit + randomNumber;
+}
+
+const refreshButton = document.getElementById("refresh_Button");
+
+refreshButton.addEventListener("click", function() {
+  getRandomCard();
+});
+
+function changeDimensions() {
+  document.getElementById("card").style.width =
+    ocument.querySelector("").value + "px";
+}
+
+/*const ChangeSize = document.getElementById("Change_Card_Size");
+
+ChangeSize.addEventListener("click", function() {
+  getRandomCard();
+});*/
